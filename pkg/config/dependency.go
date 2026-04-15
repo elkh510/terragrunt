637be776d868d516ec3c38495c8221d321807eee
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2026-04-15 16:29:08 -0400

    chore: Adding some integration testing

--- pkg/config/dependency.go
+++ pkg/config/dependency.go
@@ -1322,6 +1322,7 @@ func runTerragruntOutputJSON(ctx context.Context, pctx *ParsingContext, l log.Lo
 		BackendBootstrap:             pctx.BackendBootstrap,
 		Telemetry:                    pctx.Telemetry,
 		AuthProviderCmd:              pctx.AuthProviderCmd,
+		CASCloneDepth:                pctx.CASCloneDepth,
 	}
 
 	err = run.Run(ctx, l, runOpts, report.NewReport(), runCfg, credsGetter)
